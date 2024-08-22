cc74614da976ef9da3e8bc63dedfea9e394c3ef5
WalnutRavi <140723920+WalnutRavi@users.noreply.github.com>
2024-08-22 16:47:08 -0300

    aula115

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+var nosex=""
+var nosey=""
 function preload(){
 
 }
 function setup(){
 canvas=createCanvas(750,550);
 canvas.center()
+video=createCapture(VIDEO)
+video.hide()
+
+posenet=ml5.poseNet(video,modelLoaded)
+posenet.on("pose",gotPoses)
+}
+function modelLoaded(){
+    console.log("Modelo carregado")
+}
+function draw(){
+    image(video,0,0,750,550)
+    circle(nosex,nosey,45)
+}
+function gotPoses(results){
+    if(results.length>0){
+        // console.log(results)
+        nosex=results[0].pose.nose.x 
+        nosey=results[0].pose.nose.y 
+
+    }
 }
